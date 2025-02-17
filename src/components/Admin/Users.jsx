47ed1bc19ef73df5d42aba23acd914b7f29768a2
benitha200 <benithaiyuyisenga2002@gmail.com>
2025-02-17 15:39:05 +0200

    changes on users

--- src/components/Admin/Users.jsx
+++ src/components/Admin/Users.jsx
@@ -412,7 +412,8 @@ const Users = () => {
                     >
                       <option value="CWS_MANAGER">CWS Manager</option>
                       <option value="ADMIN">Admin</option>
-                      <option value="SUPER_ADMIN">Super Admin</option>
+                      {/* <option value="SUPER_ADMIN">Super Admin</option> */}
+                      <option value="SUPERVISOR">Supervisor</option>
                     </select>
                   </div>
                   <div className="col-md-3 mb-3">
