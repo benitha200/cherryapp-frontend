f09478b7c0188012feb3f2e7b9292a9c9d68f883
Niyonkuru Tresor <niyonkurutresor17@example.com>
2025-05-29 07:26:00 +0200

    fix report cards

--- src/components/Admin/quality/report/components/reportTable.jsx
+++ src/components/Admin/quality/report/components/reportTable.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import { columns } from "./reportColums";
-import { Card } from "react-bootstrap";
+import { Card, Form, InputGroup } from "react-bootstrap";
 import { GetReport } from "../action";
 import { Skeleton } from "./skeleton";
 import { Error } from "../../components/responses";
 import { ReprotTable } from "./reportRows";
+import { DashboardCard } from "./dashboardCard";
+import { formatNumberWithCommas } from "../../../../../utils/formatNumberWithComma";
 
 export const QualityReportTable = () => {
   const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [searchQuery, setSearchQuery] = useState("");
   const { isPending, error, data } = GetReport();
   if (error) {
     return <Error error={error?.message ?? "Failed to fetch report Data."} />;
@@ -31,67 +34,146 @@ export const QualityReportTable = () => {
     <Skeleton />
   ) : (
     !isPending && data && (
-      <div className="card">
-        <div className="card-body">
-          <div className="d-flex justify-content-between align-items-center mb-3">
-            <h4 className="mb-0">Station Quality report</h4>
+      <div>
+        <div className="row g-4 mb-4">
+          <div className="col-12 col-md-6">
+            <DashboardCard
+              title="Total Cherry Purchased (kg)"
+              value={formatNumberWithCommas(
+                data?.data?.grandTotals?.GrandtotalTransportedKgs ?? 0
+              )}
+              iconClass="bi-basket-fill"
+            />
+          </div>
+          <div className="col-12 col-md-6">
+            <DashboardCard
+              title="Total Deliverd (kg)"
+              value={formatNumberWithCommas(
+                data?.data?.grandTotals?.GrandtotalDeliveredKgs ?? 0
+              )}
+              iconClass="bi-bus-front"
+            />
+          </div>{" "}
+          <div className="col-12 col-md-6">
+            <DashboardCard
+              title="Total Average 15+ velivered (kg)"
+              value={formatNumberWithCommas(
+                data?.data?.grandTotals?.GrandtotAvg15PlusDelivery ?? 0
+              )}
+              iconClass="bi-basket-fill"
+            />
+          </div>
+          <div className="col-12 col-md-6">
+            <DashboardCard
+              title="Total Average 13/14 Delivered (kg)"
+              value={formatNumberWithCommas(
+                data?.data?.grandTotals?.GrandtotAvg1314Delivery ?? 0
+              )}
+              iconClass="bi-basket-fill"
+            />
+          </div>
+          <div className="col-12 col-md-6">
+            <DashboardCard
+              title="Total Average Lowgrade Delivered (kg)"
+              value={formatNumberWithCommas(
+                data?.data?.grandTotals?.GrandtotAVLGDelivery ?? 0
+              )}
+              iconClass="bi-battery-low"
+            />
+          </div>
+          <div className="col-12 col-md-6">
+            <DashboardCard
+              title="Total OT Delivered (kg)"
+              value={formatNumberWithCommas(
+                data?.data?.grandTotals?.GrandtotOTDelivery ?? 0
+              )}
+              iconClass="bi-highlights"
+            />
           </div>
         </div>
-        <div className="table-responsive">
-          <table
-            className="table table-hover"
-            style={{
-              borderCollapse: "separate",
-              borderSpacing: 0,
-              width: "100%",
-              border: `1px solid ${processingTheme.tableBorder}`,
-            }}
-          >
-            <thead>
-              <tr>
-                {columns.map((column, index) => (
-                  <th
-                    key={index}
-                    style={{
-                      // backgroundColor: processingTheme.tableHeader,
-                      // color: processingTheme.primary,
-                      padding: "10px 15px",
-                      // fontWeight: 400,
-                      // borderBottom: `1px solid`,
-                      whiteSpace: "nowrap",
-                    }}
-                  >
-                    {column.header || column.field}
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {data?.data?.report?.length == 0 ? (
-                <tr>
-                  <td colSpan={columns?.length}>
-                    <div className="text-center fw-bold fs-5 p-3 border border-warning rounded bg-light text-dark">
-                      {"No data available"}
-                    </div>
-                  </td>
-                </tr>
-              ) : (
-                (data?.data?.report ?? []).map((element) => (
-                  <ReprotTable
-                    data={element ?? []}
-                    columns={columns}
-                    pageSizeOptions={[5, 10, 20]}
-                    initialPageSize={5}
-                    isLoading={false}
-                    onPageSizeChange={setItemsPerPage}
-                    rowKeyField="batchNo"
-                    itemsPerPage={itemsPerPage}
-                    emptyStateMessage={"There is no data"}
+
+        <div className="card">
+          <div className="card-body">
+            <div className="d-flex justify-content-between align-items-center mb-3">
+              <h4 className="mb-0">Station Quality report</h4>
+              <div style={{ width: "250px" }}>
+                <InputGroup>
+                  <InputGroup.Text>
+                    <i className="bi bi-search"></i>
+                  </InputGroup.Text>
+                  <Form.Control
+                    type="text"
+                    placeholder="Search..."
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
                   />
-                ))
-              )}
-            </tbody>
-          </table>
+                </InputGroup>
+              </div>
+            </div>
+          </div>
+          <div className="table-responsive">
+            <table
+              className="table table-hover"
+              style={{
+                borderCollapse: "separate",
+                borderSpacing: 0,
+                width: "100%",
+                border: `1px solid ${processingTheme.tableBorder}`,
+              }}
+            >
+              <thead>
+                <tr>
+                  {columns.map((column, index) => (
+                    <th
+                      key={index}
+                      style={{
+                        // backgroundColor: processingTheme.tableHeader,
+                        // color: processingTheme.primary,
+                        padding: "10px 15px",
+                        // fontWeight: 400,
+                        // borderBottom: `1px solid`,
+                        whiteSpace: "nowrap",
+                      }}
+                    >
+                      {column.header || column.field}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {data?.data?.report?.length == 0 ? (
+                  <tr>
+                    <td colSpan={columns?.length}>
+                      <div className="text-center fw-bold fs-5 p-3 border border-warning rounded bg-light text-dark">
+                        {"No data available"}
+                      </div>
+                    </td>
+                  </tr>
+                ) : (
+                  (!searchQuery
+                    ? data?.data?.report
+                    : data?.data?.report?.filter((element) =>
+                        element?.cws?.name
+                          ?.toLowerCase()
+                          ?.includes(searchQuery.toLowerCase())
+                      )
+                  ).map((element) => (
+                    <ReprotTable
+                      data={element ?? []}
+                      columns={columns}
+                      pageSizeOptions={[5, 10, 20]}
+                      initialPageSize={5}
+                      isLoading={false}
+                      onPageSizeChange={setItemsPerPage}
+                      rowKeyField="batchNo"
+                      itemsPerPage={itemsPerPage}
+                      emptyStateMessage={"There is no data"}
+                    />
+                  ))
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     )
