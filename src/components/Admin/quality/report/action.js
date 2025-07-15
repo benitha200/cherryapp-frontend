56316658472a4bc5d91c30fea304cfc7cd29f0ec
YVES <100066447+iraduyves@users.noreply.github.com>
2025-07-15 17:47:15 +0200

    feat: intergrate the Delivery report with transit

--- src/components/Admin/quality/report/action.js
+++ src/components/Admin/quality/report/action.js
@@ -1,9 +1,18 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { report } from "./../../../../apis/qualityReport";
+import { report, deliveryReportDadta } from "./../../../../apis/qualityReport";
 export const GetReport = (id) => {
   const { isPending, error, data } = useQuery({
     queryKey: ["Report"],
     queryFn: async () => await report(),
   });
   return { isPending, error, data };
+};
+
+
+export const GetReportData = () => {
+  const { isPending: deliveryPending, error, data: deliverydata } = useQuery({
+    queryKey: ["ReportDeliveryData"],
+    queryFn: async () => await deliveryReportDadta(),
+  });
+  return { deliveryPending, error, deliverydata };
 };
